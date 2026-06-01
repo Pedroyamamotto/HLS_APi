7ec83ebaea7f2edd37a698335105121e045f0d52
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2026-06-01 09:22:56 -0300

    Accept larger hotel image payloads

--- src/server/server.js
+++ src/server/server.js
@@ -26,8 +26,8 @@ const io = new Server(httpServer, {
 
 // Configurar CORS para refletir a origem e permitir credenciais (cookies)
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use('/pages', express.static(path.join(__dirname, '..', 'public', 'pages')));
 
 // Documentação Swagger
